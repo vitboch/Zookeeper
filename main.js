eace4a8842f953224f36ab50cdf21e818930c0d5
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-05-18 12:00:50 +0700

    2 part

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-console.log(`I love animals!
-Let's check on the animals...
-The deer looks fine.
-The bat looks happy.
-The lion looks healthy.`)
+animal = `
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \\    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!`
+
+console.log(animal)
